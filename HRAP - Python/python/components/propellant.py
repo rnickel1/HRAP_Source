7b3976f037b0218c13497444948afdd94c4a3667
Jessica Kuleshov <jkuleshov2@gmail.com>
2023-07-03 18:46:27 -0400

    Added in regression model parameters

--- HRAP - Python/python/components/propellant.py
+++ HRAP - Python/python/components/propellant.py
@@ -5,13 +5,14 @@ Propellant. This can be either loaded in or taken from UI
 
 class Propellant:
 
-    def __init__(self, name, density, const_of, const_of_ratio, c_star_eff, grain_id, grain_od, grain_length):
+    def __init__(self, name, density, c_star_eff, grain_id, grain_od,
+                 grain_length, const_of_ratio=None, c_regression=None, exp_regression=None, exp_length=None):
         self.name = name
         self.density = density  # Default kg/m^3
-        if const_of:  # Regression model is Constant OF
-            self.const_of_ratio = const_of_ratio
-        else:  # Regression model is Shifting OF
-            self.const_of_ratio = None
+        self.const_of_ratio = const_of_ratio  # Constant OF only
+        self.c_regression = c_regression  # mm/s, Regression only
+        self.exp_regression = exp_regression  # kg/m^2-s, Regression only
+        self.exp_length = exp_length  # m, Regression only
         self.c_star_eff = c_star_eff  # C* efficiency, percent
         self.grain_id = grain_id  # Default cm
         self.grain_od = grain_od  # Default cm
@@ -19,4 +20,6 @@ class Propellant:
 
 
 def load_from_csv(filepath):
+    # You can also load from CSV! headers should be:
+    # name,density,const_of,const_of_ratio,c_star_eff,grain_id,grain_od,grain_length
     return Propellant()
--- HRAP - Python/python/components/sim_config.py
+++ HRAP - Python/python/components/sim_config.py
@@ -5,7 +5,7 @@ Simulation configuration (contains all other models)
 
 class SimulationConfiguration:
     def __init__(self, tank_temp, chamber_pressure, oxidizer_mass, ambient_press,
-                 max_sim_runtime, max_burn_time, sim_timestep):
+                 max_sim_runtime, max_burn_time, sim_timestep, regression_model):
         self.tank_temp = tank_temp  # Default K. TODO: account for ppl inputting pressure instead
         self.chamber_pressure = chamber_pressure  # Default atm
         self.starting_ox_mass = oxidizer_mass  # Default kg. TODO: account for tank fill percentage input
@@ -14,5 +14,5 @@ class SimulationConfiguration:
         self.max_sim_runtime = max_sim_runtime  # seconds
         self.max_burn_time = max_burn_time  # seconds
         self.sim_timestep = sim_timestep  # seconds
-        # Add regression or constant OF here???
+        self.regression_model = regression_model
 
